37ada0641ab5309c44fdadeb44fc9f4cd6cc0a78
cgaffney <codygaffney94@gmail.com>
2022-02-21 18:11:22 +0000

    added blank value to dropdowns, put a link from php back to html

--- PartA/toppings.php
+++ PartA/toppings.php
@@ -1,9 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
     $topping1 = $topping2 = $topping3 = $phone = "";
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $topping1 = test_input($_POST["topping1"]);
-        $topping2 = test_input($_POST["topping2"]);
-        $topping3 = test_input($_POST["topping3"]);
+    if($_SERVER["REQUEST_METHOD"] == "GET"){
+        if($_POST['topping1'] != '' && $_POST['topping2'] != '' && $_POST['topping3'] != ''){
+            $topping1 = test_input($_POST["topping1"]);
+            $topping2 = test_input($_POST["topping2"]);
+            $topping3 = test_input($_POST["topping3"]);
+        }
+        else{
+            echo "Please make sure all option are selected";
+
+        }
+        
     }
 
     function test_input($data) {
@@ -13,3 +29,6 @@
         return $data;
       }
 ?>
+<p>Click <a href="/~s3049238/ass01/PartA/toppings.html">here</a> to go back</p>
+</body>
+</html>
